6dfab9492464817b044516cc72e5714ab457a670
K. A. Simms <46714075+Kasmanian@users.noreply.github.com>
2022-12-11 23:24:04 -0500

    Create be_test.py

---
+++ be_test.py
@@ -0,0 +1,17 @@
+import unittest
+from db import Database
+
+class TestDatabase(unittest.TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self.db = Database()
+
+    def test_connect_fail(self):
+        is_connected = self.db.connect('')
+        self.assertEqual(is_connected, False, 'Connection should fail')
+
+    # def test_connect_pass(self):
+    #     pass
+
+if __name__ == '__main__':
+    unittest.main()
