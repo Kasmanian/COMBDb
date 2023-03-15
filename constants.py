e055b25491060ab7f8a8f340c99a5651102e7ef7
Kasmanian <ksimms7830@gmail.com>
2023-03-14 21:10:14 -0400

    Added app data.

---
+++ constants.py
@@ -0,0 +1,8 @@
+CULTURE_ORDER_KWARGS = {
+    'table': '[Cultures]',
+    'fields': (
+            '[SampleID]', '[ChartID]', '[Clinician]', '[First]', '[Last]', 
+            '[Received]', '[Collected]', '[Reported]', '[Aerobic Results]', '[Anaerobic Results]',
+            '[Tech]', '[Comments]', '[Notes]', '[Type]', '[Direct Smear]', '[Rejection Date]', '[Rejection Reason]'
+            ),
+}
